6b52f27e3c14d15b57a6e84f4d1813901f204e4d
kunc <kurookun@gmail.com>
2020-03-24 14:34:28 +0700

    add authors and new author page2

--- routes/authors.js
+++ routes/authors.js
@@ -1,19 +1,39 @@
 const express = require('express')
 const router = express.Router()
+const Author = require('../models/author')
 
 // All Authors Route
-router.get('/', (req, res) => {
-    res.render('authors/index')
+router.get('/', async (req, res) => {
+    try {
+        const authors = await Author.find({})
+        res.render('authors/index', {authors: authors})
+    } catch {
+        res.redirect('/')
+    }
 })
+
+
 
 // New Authors Route
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 })
 
 // Create Authors Route
-router.get('/', (req, res) => {
-    res.send('Create')
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating author'
+        })
+    }
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
-const express = require("express")
+const express = require('express')
 const app = express()
-const expressLayouts = require("express-ejs-layouts")
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
@@ -14,9 +15,13 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ 
+    limit: '10mb',
+    extended: false
+}))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Monggose'))
